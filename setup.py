b5fb334c3253f52e76cd20e60d022504bd9cf850
pradeeptomer4u <pradeeptomer4u@gmail.com>
2024-06-07 12:53:35 +0000

    changes

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tracking_sdk',
-    version='0.1.0',
+    name='tracking-sdk',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/pradeeptomer4u/tracking-sdk',
-    license='MIT',
+    license=None,
     author='Pradeep',
     author_email='pradeeptomer4u@gmail.com',
-    description='A lightweight SDK for tracking events',
+    description='A lightweight SDK for tracking events in Python applications',
+    long_description='A Python SDK for tracking user events, providing easy integration and usage in Python applications.',
     python_requires='>=3.10',
     install_requires=['requests'],
 )
